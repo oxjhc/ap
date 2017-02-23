04e2303b93e317590167aec23774965f069b4236
Andrew Kenyon-Roberts <andrew_kr@btinternet.com>
2017-02-23 19:48:30 +0000

    Implement finite field of prime order
    Create an instance of Fractional corresponding to F65521.
    It could be easily changed to a different prime, but the prime
    used is hard-coded in.

---
+++ server/Number.hs
@@ -0,0 +1,36 @@
+module Number(
+) where
+
+import Data.Ratio
+
+-- It would be nice to parameterise over this, but that would require the modulus to be part of the type of the numbers, which I can't work out how to do without just using type-level unary number representations, which would be awful for efficiency.
+p :: Int
+p = 65521
+
+-- Ord is only included for tree purposes. It has no algebraic significance.
+newtype PrimeField = PF {unPF :: Int} deriving (Eq, Ord)
+
+instance Num PrimeField where
+    (PF a) + (PF b) = PF $ mod (a + b) p
+    (PF a) - (PF b) = PF $ mod (a - b) p
+    (PF a) * (PF b) = PF $ mod (a * b) p
+    abs a = a -- abs doesn't make sense at all for finite fields.
+    signum _ = 1
+    fromInteger n = PF $ mod (fromInteger n) p
+
+instance Fractional PrimeField where
+    -- the fromRational definition doesn't entirely make sense but apparently it's compulsory.
+    fromRational r = fromInteger (numerator r) / fromInteger (denominator r)
+    recip (PF a) = if u == 1 then PF i else error "divide by zero"
+        where (u, _, i) = bézout p a
+
+bézout :: Integral n => n -> n -> (n, n, n)
+bézout x y -- = (u, a, b) where u = a*x + b*y, assuming x > y
+  | y == 0    = (x, 1, 0)
+  | otherwise = (u, b, a-q*b)
+      where (q, r) = divMod x y
+            (u, a, b) = bézout y r
+
+instance Show PrimeField where
+    show (PF n) = show $ mod (n+o) p - o
+        where o = div p 2
