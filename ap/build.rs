bd475bebd0eabd753b68e7ff3ec76a595ec62f91
Sauyon Lee <sauyonl@sauyon.com>
2017-02-26 16:45:10 +0000

    working build script for protobuf

---
+++ ap/build.rs
@@ -0,0 +1,11 @@
+use std::process::Command;
+
+fn main() {
+  println!("cargo:rerun-if-changed=../protos/messages.proto");
+  let output = Command::new("protoc")
+    .arg("--rust_out")
+    .arg("src")
+    .arg("-I").arg("../protos")
+    .arg("../protos/messages.proto")
+    .output().unwrap();
+}
--- ap/src/lib.rs
+++ ap/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate rand;
+extern crate protobuf;
 
 mod point;
 mod galois_field;
 pub mod vault;
+mod messages;
