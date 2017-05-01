a775014b1e3246a4f0cf03d0b48397a99c61ef06
Sauyon Lee <sauyonl@sauyon.com>
2017-05-01 19:26:01 +0100

    working signing

--- ap/src/main.rs
+++ ap/src/main.rs
@@ -1,21 +1,34 @@
 extern crate ap;
+extern crate chan;
 extern crate hyper;
 extern crate rand;
-extern crate ring;
+extern crate openssl;
 extern crate protobuf;
+//extern crate wpactrl;
 
 use std::{thread, time};
-use std::net::UdpSocket;
-use std::os::unix::net::UnixStream;
+use std::net::{UdpSocket, SocketAddr};
 use std::env;
 use std::process::exit;
+use std::sync::{Mutex};
 
-use hyper::server::{Server, Request, Response};
+use chan::{Sender, Receiver};
+
+use hyper::server::{Server, Handler, Request, Response};
 use hyper::client::Client;
 use hyper::status::StatusCode;
 use hyper::uri::RequestUri;
 use hyper::header;
-use hyper::mime::{Mime, TopLevel, SubLevel};
+
+use openssl::bn::BigNumContext;
+use openssl::sign::{Signer, Verifier};
+use openssl::ec::{EcGroup, EcPoint, EcKey};
+use openssl::pkey::PKey;
+use openssl::nid::X9_62_PRIME256V1 as CURVE;
+use openssl::hash::MessageDigest;
+use openssl::error::ErrorStack;
+
+//use wpactrl::WpaCtrl;
 
 use rand::Rng;
 use rand::os::OsRng;
@@ -23,83 +36,180 @@ use rand::os::OsRng;
 use protobuf::Message;
 
 use ap::vault::make_vault;
-use ap::messages::{ProofReq, SignedProofReq,
+use ap::messages::{SignedProofReq,
                    ProofResp, SignedProofResp,
-                   VaultMsg, SignedVaultMsg};
+                   VaultMsg, SignedVaultMsg,
+                   LocnProof, SignedLocnProof};
 
 static SERVER_URL: &'static str = "http://oxjhc.club";
+//static seq_ids: [u64; 2] = [ 0, 0 ]
+static mut SEQID: i64 = 0;
 
-fn ping(seq: u64, sock: &UdpSocket) {
+fn ping(sock: &UdpSocket) {
+  let seq = unsafe { SEQID };
   // convert apid and seq to a byte array
   let data : [u8; 8] = [
     (seq >> 56) as u8, (seq >> 48) as u8, (seq >> 40) as u8, (seq >> 32) as u8,
     (seq >> 24) as u8, (seq >> 16) as u8, (seq >> 8) as u8, seq as u8
   ];
-  sock.send_to(&data, "255.255.255.255:1832").unwrap();
+  match sock.send_to(&data, "255.255.255.255:1832") {
+    Ok(_) => (),
+    Err(err) => println!("{}", err)
+  }
 }
 
-fn handle_req(mut req: Request, mut res: Response) {
-  match req.uri.clone() {
-    RequestUri::AbsolutePath(ref path) => match (&req.method, &path[..]) {
-      (&hyper::Post, "/proof_req") => {
-        // generate nonce
-        let mut nonce = Vec::<u8>::new();
-        nonce.reserve(10);
-        let mut rand = OsRng::new().unwrap();
-        for _ in 1..10 { nonce.push(rand.gen()); }
+fn spawn_pinger() {
+  // this... should be modifiable at runtime.
+  // buuuuuuuut I'm lazy
+  let pinger = UdpSocket::bind("0.0.0.0:1832").unwrap();
+  pinger.set_broadcast(true).unwrap();
 
-        // TODO: unwrapping here is BAD
-        let mut sgn_prf_req: SignedProofReq = protobuf::parse_from_reader(&mut req).unwrap();
-        let prf_req = sgn_prf_req.take_proofreq();
+  thread::spawn(move || {
+    let mut rand = OsRng::new().unwrap();
+    loop {
+      thread::sleep(time::Duration::from_millis(100));
+      unsafe { SEQID = rand.gen::<i64>(); }
+      ping(&pinger);
+    }
+  });
+}
 
-        *res.status_mut() = StatusCode::NotImplemented;
-        res.send(b"This ap sucks right now. It probably sent a fake response to the server.").unwrap();
 
-        gen_proof(nonce);
-      },
-      _ => {
-        *res.status_mut() = StatusCode::NotFound;
-        res.send(b"No such resource! Don't be stupid!").unwrap();
-      }
-    },
-    _ => {
-      *res.status_mut() = StatusCode::NotImplemented;
-      res.send(b"Please post to this server.").unwrap();
+fn key_from_bytes(bytes: &[u8]) -> Result<PKey, ErrorStack> {
+  let group = EcGroup::from_curve_name(CURVE).unwrap();
+  let mut ctx = BigNumContext::new().unwrap();
+  let point = EcPoint::from_bytes(&group, bytes, &mut ctx).unwrap();
+  let key = try!(EcKey::from_public_key(&group, &point));
+  PKey::from_ec_key(key)
+}
+
+struct Dormouse {
+  key: PKey,
+  prflock: Mutex<()>,
+  tx: Sender<()>,
+  rx: Receiver<()>
+}
+
+impl Dormouse {
+  fn new(key: PKey) -> Dormouse {
+    let lock = Mutex::new(());
+    let (tx, rx) = chan::sync(0);
+    Dormouse{key: key, prflock: lock, tx: tx, rx: rx}
+  }
+
+  fn gen_proof(&self, uid: &[u8], unonce: &[u8], nonce: &[u8]) {
+    let locn_tag = vec![ 243, 122, 33, 214 ];
+    let vault = make_vault(locn_tag, 10, 100);
+
+    let mut msg = VaultMsg::new();
+    msg.set_vault(vault);
+    msg.set_uid(uid.to_vec());
+    msg.set_unonce(unonce.to_vec());
+    msg.set_apid(self.key.public_key_to_der().unwrap());
+    msg.set_apnonce(nonce.to_vec());
+    msg.set_time(time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs());
+
+    let mut signer = Signer::new(MessageDigest::sha256(), &self.key).unwrap();
+    signer.update(msg.write_to_bytes().unwrap().as_slice()).unwrap();
+    let mut sgn_msg = SignedVaultMsg::new();
+    sgn_msg.set_vault_msg(msg);
+    sgn_msg.set_sig(signer.finish().unwrap());
+
+    // send proof back to client (after connecting via wifi-direct and
+    // generating proof
+
+    // I really need a better error handling method
+    let client = Client::new();
+    println!("sending vault to server");
+    let resp = client.post(format!("{}{}", SERVER_URL, "/vault").as_str())
+      .header(header::ContentType("application/x-protobuf".parse().unwrap()))
+      .body(sgn_msg.write_to_bytes().unwrap().as_slice())
+      .send().unwrap();
+    if resp.status == hyper::Ok {
+      println!("vault successfully sent");
+      // done.
+    } else {
+      println!("error sending vault: {}", resp.status);
     }
   }
 }
 
-fn gen_proof(nonce: Vec<u8>) {
-  let locn_tag = vec![ 243, 122, 33, 214 ];
-  let vault = make_vault(locn_tag, 10, 100);
+impl Handler for Dormouse {
+  fn handle(&self, mut req: Request, mut res: Response) {
+    match req.uri.clone() {
+      RequestUri::AbsolutePath(ref path) => match (&req.method, &path[..]) {
+        (&hyper::Post, "/proof_req") => {
+          // lock proof lock, ensuring only one proof generation occurs at one time
+          let _ = self.prflock.lock().unwrap();
 
-  let mut msg = VaultMsg::new();
-  msg.set_vault(vault);
-  msg.set_uid(vec![0x12]);
-  msg.set_unonce(vec![0x34]);
-  msg.set_apid(vec![0x56]);
-  msg.set_apnonce(nonce);
-  msg.set_time(time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs());
+          let mut sgn_prf_req: SignedProofReq = match protobuf::parse_from_reader(&mut req) {
+            Ok(res) => res,
+            Err(_) => {
+              *res.status_mut() = StatusCode::BadRequest;
+              res.send(b"Fuck you don't send me invalid protobufs").unwrap();
+              return;
+            }
+          };
+          let mut prf_req = sgn_prf_req.take_proofreq();
 
-  let mut sgn_msg = SignedVaultMsg::new();
-  sgn_msg.set_vault_msg(msg);
-  sgn_msg.set_sig(vec![0x78]);
+          if prf_req.get_seqid() != unsafe { SEQID } {
+            *res.status_mut() = StatusCode::Unauthorized;
+            res.send(b"The sequence id sent is too old").unwrap();
+            return;
+          }
 
-  // send proof back to client (after connecting via wifi-direct and
-  // generating proof
+          // lol more unwrapping
+          let ukey = key_from_bytes(prf_req.get_uid()).unwrap();
+          let mut verifier = Verifier::new(MessageDigest::sha256(), &ukey).unwrap();
+          verifier.update(prf_req.write_to_bytes().unwrap().as_slice()).unwrap();
+          if verifier.finish(sgn_prf_req.take_sig().as_slice()).unwrap() {
+            let mut prf_res = ProofResp::new();
+            prf_res.set_uid(prf_req.take_uid());
+            prf_res.set_unonce(prf_req.take_unonce());
 
-  // I really need a better error handling method
-  let client = Client::new();
-  println!("sending vault to server");
-  let resp = client.post(format!("{}{}", SERVER_URL, "/vault").as_str())
-    .header(header::ContentType("application/x-protobuf".parse().unwrap()))
-    .body(sgn_msg.write_to_bytes().unwrap().as_slice())
-    .send().unwrap();
-  if resp.status == hyper::Ok {
-    println!("vault successfully sent");
-    // done.
-  } else {
-    println!("error sending vault: {}", resp.status);
+            let mut signer = Signer::new(MessageDigest::sha256(), &self.key).unwrap();
+            signer.update(prf_res.write_to_bytes().unwrap().as_slice()).unwrap();
+            let mut sgn_prf_res = SignedProofResp::new();
+            sgn_prf_res.set_proofresp(prf_res.clone());
+            sgn_prf_res.set_sig(signer.finish().unwrap());
+
+            *res.status_mut() = StatusCode::Ok;
+            res.send(sgn_prf_res.write_to_bytes().unwrap().as_slice()).unwrap();
+
+            // generate nonce
+            let mut nonce = Vec::<u8>::new();
+            nonce.reserve(10);
+            let mut rand = OsRng::new().unwrap();
+            for _ in 1..10 { nonce.push(rand.gen()); }
+
+            self.gen_proof(prf_res.get_uid(), prf_res.get_unonce(), nonce.as_slice());
+
+            self.rx.recv();
+          } else {
+            *res.status_mut() = StatusCode::Forbidden;
+            res.send(b"That signature doesn't match your public key!").unwrap();
+          }
+        },
+        (&hyper::Get, "/proof") => {
+          match self.prflock.try_lock() {
+            Ok(_) => {
+              *res.status_mut() = StatusCode::Unauthorized;
+              res.send(b"no request in progress").unwrap();
+            },
+            Err(_) => {}
+          }
+          self.tx.send(());
+        },
+        _ => {
+          *res.status_mut() = StatusCode::NotFound;
+          res.send(b"No such resource! Don't be stupid!").unwrap();
+        }
+      },
+      _ => {
+        *res.status_mut() = StatusCode::NotImplemented;
+        res.send(b"Please post to this server.").unwrap();
+      }
+    }
   }
 }
 
@@ -116,22 +226,15 @@ fn ping_server() {
 }
 
 fn main() {
-  // spawn pinger
-  // TODO: this... should be modifiable at runtime.
-  // buuuuuuuut I'm lazy
-  let pinger = UdpSocket::bind("127.0.0.1:1832").unwrap();
-  pinger.set_broadcast(true).unwrap();
-  thread::spawn(move || {
-    let mut rand = OsRng::new().unwrap();
-    loop {
-      thread::sleep(time::Duration::from_millis(100));
-      ping(rand.gen(), &pinger);
-    }
-  });
+  let key = EcKey::from_curve_name(CURVE).unwrap();
+  let pkey = PKey::from_ec_key(key).unwrap();
+  let dormouse = Dormouse::new(pkey);
+
+  spawn_pinger();
 
   for arg in env::args() {
     if arg == "-test" {
-      gen_proof(vec![0x00]);
+      dormouse.gen_proof(&[0x00], &[0x11], &[0x22]);
       exit(0);
     } else if arg == "-ping" {
       ping_server();
@@ -139,5 +242,5 @@ fn main() {
     }
   }
 
-  Server::http("0.0.0.0:1865").unwrap().handle(handle_req).unwrap();
+  Server::http("0.0.0.0:1865").unwrap().handle(dormouse).unwrap();
 }
