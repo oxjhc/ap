bcc1da31160cca18c434bd323088ffe3bada9f49
Sauyon Lee <sauyonl@sauyon.com>
2017-05-09 10:50:52 +0100

    use Command for wpa_supplicant control. closes #6

--- ap/src/main.rs
+++ ap/src/main.rs
@@ -1,15 +1,11 @@
 extern crate ap;
 extern crate hyper;
-extern crate untrusted;
-extern crate ring;
-extern crate openssl;
-extern crate toml;
-extern crate base64;
-extern crate get_if_addrs;
 //extern crate wpactrl;
 
 use std::env;
-use std::process::exit;
+use std::fs::File;
+use std::io::Write;
+use std::process::{Command, exit};
 
 use hyper::server::Server;
 
@@ -29,6 +25,41 @@ fn main() {
       exit(0);
     }
   }
+
+  println!("flushing p2p state");
+  let out = Command::new("wpa_cli").arg("p2p_flush").output().unwrap();
+  println!("stdout: {}", String::from_utf8(out.stdout).unwrap());
+  println!("stderr: {}", String::from_utf8(out.stderr).unwrap());
+  println!("flushing p2p services");
+  let out = Command::new("wpa_cli").arg("p2p_service_flush").output().unwrap();
+  println!("stdout: {}", String::from_utf8(out.stdout).unwrap());
+  println!("stderr: {}", String::from_utf8(out.stderr).unwrap());
+  println!("adding service");
+  let out = Command::new("wpa_cli")
+    .arg("p2p_service_add").arg("upnp").arg("10")
+    .arg("uuid:Dormouse::urn:schemas-oxjhc-club:service:TeaParty:1")
+    .output().unwrap();
+  println!("stdout: {}", String::from_utf8(out.stdout).unwrap());
+  println!("stderr: {}", String::from_utf8(out.stderr).unwrap());
+  println!("starting p2p listen");
+  let out = Command::new("wpa_cli").arg("p2p_listen").output().unwrap();
+  println!("stdout: {}", String::from_utf8(out.stdout).unwrap());
+  println!("stderr: {}", String::from_utf8(out.stderr).unwrap());
+
+  println!("spawning p2p controller");
+  // TODO: use tempfile
+  let mut tmpscript = File::create("tmp.sh").unwrap();
+  tmpscript.write_all(br#"#!/bin/bash
+journalctl -flu wpa_supplicant-nl80211@wlan0 -n0 |
+  stdbuf -oL sed -n 's/.*P2P-GO-NEG-REQUEST\ \([0-9a-f:]*\)\ .*/\1/p' |
+  while IFS= read -r line; do
+    echo "connecting to $line"
+    wpa_cli p2p_connect "$line" pbc
+  done
+"#).unwrap();
+  Command::new("bash")
+    .arg("tmp.sh")
+    .spawn().unwrap();
 
   println!("starting dormouse");
   Server::http(format!("0.0.0.0:{}", dormouse.cfg.http_port)).unwrap().handle(dormouse).unwrap();
